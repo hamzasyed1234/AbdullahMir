273a69f676e5a11b879ec35a8836fee2903f4ff0
Hamza Syed <hamzasy416@gmail.com>
2026-05-22 15:45:14 -0400

    yo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
 import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import AdminBar from './components/AdminBar'
 import ScrollToTop from './components/ScrollToTop'
-import Home from './pages/Home'
-import MeetMir from './pages/MeetMir'
-import Priorities from './pages/Priorities'
-import Articles from './pages/Articles'
-import Press from './pages/Press'
-import AdminLogin from './pages/AdminLogin'
+
+const Home = lazy(() => import('./pages/Home'))
+const MeetMir = lazy(() => import('./pages/MeetMir'))
+const Priorities = lazy(() => import('./pages/Priorities'))
+const Articles = lazy(() => import('./pages/Articles'))
+const Press = lazy(() => import('./pages/Press'))
+const AdminLogin = lazy(() => import('./pages/AdminLogin'))
 
 export default function App() {
   return (
@@ -18,14 +20,16 @@ export default function App() {
         <ScrollToTop />
         <AdminBar />
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/meet-mir" element={<MeetMir />} />
-          <Route path="/priorities" element={<Priorities />} />
-          <Route path="/articles" element={<Articles />} />
-          <Route path="/press" element={<Press />} />
-          <Route path="/admin-login" element={<AdminLogin />} />
-        </Routes>
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/meet-mir" element={<MeetMir />} />
+            <Route path="/priorities" element={<Priorities />} />
+            <Route path="/articles" element={<Articles />} />
+            <Route path="/press" element={<Press />} />
+            <Route path="/admin-login" element={<AdminLogin />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </BrowserRouter>
     </AuthProvider>
